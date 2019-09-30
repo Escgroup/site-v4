5f5875cb5f1294dc88808316a6040a44eb0aacea
mouse <escmouse484@gmail.com>
2019-09-30 22:33:11 +0900

    ✨ : Add About

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,16 @@
             <p class="h3">インターネット上の脱出口</p>
             <p class="h1">[Esc]</p>
         </div>
+        <b-row class="mt-5 mx-5 p-4">
+            <b-col class="text-left" sm>
+                <p class="h4">[Esc] とは</p>
+                <p>
+                    Escape（エスケープ）の略です。
+                    Escapeは日本語で逃げるという意味です。
+                    このグループは何かから逃げ出したいなと思ったときの脱出口、憩いの場のような場所を目指しています。
+                </p>
+            </b-col>
+            <b-col class="bg-info" sm> </b-col>
+        </b-row>
     </div>
 </template>
