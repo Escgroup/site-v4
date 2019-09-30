d970f32ed552a722f2b1e00d5232ef7f25932ea5
mouse <escmouse484@gmail.com>
2019-09-30 15:25:48 +0900

    🎨 : Change eslint Rules

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 4,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  printWidth: 80,
+  endOfLine: "lf",
+};
