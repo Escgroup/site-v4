1986eb04d6751016d326b35f45512bb14376c373
mouse <escmouse484@gmail.com>
2019-09-30 16:07:27 +0900

    ✨ : Add Header

---
+++ components/header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <b-navbar toggleable="lg">
+            <nuxt-link to="/">
+                <b-navbar-brand>[Esc] Group site</b-navbar-brand>
+            </nuxt-link>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-navbar-nav class="mr-sm-5">
+                        <b-nav-text class="mr-3">
+                            <nuxt-link to="/">Home</nuxt-link>
+                        </b-nav-text>
+                        <b-nav-text>
+                            <nuxt-link to="/Community">Community</nuxt-link>
+                        </b-nav-text>
+                    </b-navbar-nav>
+
+                    <b-nav-item-dropdown text="Lang" right>
+                        <b-dropdown-item href="#">JA</b-dropdown-item>
+                        <b-dropdown-item href="#">EN</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
