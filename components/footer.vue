cd65568dd3146c27eaf03b483af1a225ef10df1f
mouse <escmouse484@gmail.com>
2019-09-30 23:02:31 +0900

    ✨ : Add Footer

---
+++ components/footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-center mb-2">
+        ©2019 [Esc] Group
+    </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
         <Header />
-        <Nuxt />
+        <div class="main">
+            <Nuxt />
+        </div>
+        <Footer />
     </div>
 </template>
 
 <script>
+import Footer from "../components/footer.vue";
 import Header from "../components/header.vue";
 export default {
-    components: { Header },
+    components: { Header, Footer },
 };
 </script>
 
 <style scoped>
 div {
     font-family: "M PLUS Rounded 1c";
+}
+.main {
+    height: 100vh;
 }
 </style>
