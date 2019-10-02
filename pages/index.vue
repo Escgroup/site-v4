96c34f28b9ce55430767f37a8f17f9cc902b9a7d
mouse <escmouse484@gmail.com>
2019-10-02 17:01:34 +0900

    ✨ : Add Background img

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="mt-5">
+    <div id="bg-net" class="mt-5 pt-5 text-dark mx-4 rounded">
         <div class="text-center">
             <p class="h2">インターネット上の脱出口</p>
             <p class="h1">[Esc]</p>
         </div>
-        <b-row class="mt-5 mx-5 p-4">
+        <b-row class="mt-5 mx-3 p-4">
             <b-col class="text-left" sm>
                 <p class="h2">[Esc] とは</p>
                 <p class="h4 mt-3 justify">
@@ -13,7 +13,16 @@
                     このグループは何かから逃げ出したいなと思ったときの脱出口、憩いの場のような場所を目指しています。
                 </p>
             </b-col>
-            <b-col class="bg-info" sm> </b-col>
+            <b-col class="pb-5" sm />
         </b-row>
     </div>
 </template>
+
+<style scoped>
+#bg-net {
+    background-image: url("~assets/img/network.jpg");
+    background-size: auto;
+    background-color: rgba(255, 255, 255, 0.7);
+    background-blend-mode: lighten;
+}
+</style>
