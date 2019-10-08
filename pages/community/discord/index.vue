2b82a4eab7546fcb0cf44cbbf656979b136be3f9
mouse484 <escmouse484@gmail.com>
2019-10-08 15:18:17 +0900

    ✨ : Add nuxt-link

---
+++ pages/community/discord/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        discord
+    </div>
+</template>
--- pages/community/index.vue
+++ pages/community/index.vue
@@ -7,6 +7,7 @@
                 :key="list"
                 style="position: relative;"
                 md="6"
+                class="m-1"
             >
                 <BgImg class="p-2 rounded" :img="path">
                     <div>
@@ -17,14 +18,16 @@
                             </b-badge>
                         </h4>
                         <p>{{ list.text }}</p>
-                        <div style="height:2em;"></div>
+                        <div class="p-3"></div>
                         <div style="position: absolute; bottom: 0.5em;">
                             <b-button :disabled="list.cos">
                                 参加する
                             </b-button>
-                            <b-button :disabled="list.cos">
-                                詳しく見る
-                            </b-button>
+                            <nuxt-link :to="list.link[0]" append>
+                                <b-button :disabled="list.cos">
+                                    詳しく見る
+                                </b-button>
+                            </nuxt-link>
                         </div>
                     </div>
                 </BgImg>
@@ -47,12 +50,14 @@ export default {
                     cos: false,
                     text:
                         "Discord上のコミュニティです。ゲーム、プログラムを中心に色々な活動をしています。基本的に毎日VCしています。",
+                    link: ["discord"],
                 },
                 {
                     name: "LINE",
                     cos: true,
                     text:
                         "作成、用意中です。公開出来たらDiscordのコミュニティの方で告知させていただきます。",
+                    link: ["line"],
                 },
             ],
             path: "/img/ai-think.jpg",
