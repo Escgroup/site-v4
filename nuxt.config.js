0eebe337ab2f7ee3f63cbf76d5a83a5afc2ca825
mouse <escmouse484@gmail.com>
2019-10-16 22:50:19 +0900

    🎨 : Change descriptions

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
      ** Headers of the page
      */
     head: {
-        title: process.env.npm_package_name || "",
+        title: "[Esc] Site",
         meta: [
             { charset: "utf-8" },
             {
@@ -14,7 +14,7 @@ export default {
             {
                 hid: "description",
                 name: "description",
-                content: process.env.npm_package_description || "",
+                content: "インターネットの脱出口[Esc]公式サイト",
             },
         ],
         link: [
